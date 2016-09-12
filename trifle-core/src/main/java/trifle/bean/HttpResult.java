666649a2852b86f42a980a7273dcede80712b825
kostark <10692081@qq.com>
2016-09-13 00:48:35 +0800

    2016-09-13

--- trifle-core/src/main/java/trifle/bean/HttpResult.java
+++ trifle-core/src/main/java/trifle/bean/HttpResult.java
@@ -1,7 +1,78 @@
 package trifle.bean;
 
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
 /**
+ * http result
  * Created by Stark on 2016/9/11.
  */
-public class HttpResult {
+public class HttpResult<T extends Serializable> {
+	private boolean success;
+
+	private String code;
+	private String subCode;
+
+	private String message;
+	private String subMessage;
+
+	private List<T> data;
+	private Map<String, String> params;
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getSubCode() {
+		return subCode;
+	}
+
+	public void setSubCode(String subCode) {
+		this.subCode = subCode;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getSubMessage() {
+		return subMessage;
+	}
+
+	public void setSubMessage(String subMessage) {
+		this.subMessage = subMessage;
+	}
+
+	public List<T> getData() {
+		return data;
+	}
+
+	public void setData(List<T> data) {
+		this.data = data;
+	}
+
+	public Map<String, String> getParams() {
+		return params;
+	}
+
+	public void setParams(Map<String, String> params) {
+		this.params = params;
+	}
 }
